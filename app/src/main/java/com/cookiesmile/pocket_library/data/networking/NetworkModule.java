c61acd5d65df1be10e3fe50c93c916a5c12c0e88
rudinesurya <rudinesurya@gmail.com>
2019-11-20 18:33:37 +0000

    retrofit caching

--- app/src/main/java/com/cookiesmile/pocket_library/data/networking/NetworkModule.java
+++ app/src/main/java/com/cookiesmile/pocket_library/data/networking/NetworkModule.java
@@ -1,5 +1,11 @@
 package com.cookiesmile.pocket_library.data.networking;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.File;
+
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -7,16 +13,117 @@ import dagger.Module;
 import dagger.Provides;
 import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Cache;
 import okhttp3.Call;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
+import timber.log.Timber;
 
 @Module
 abstract class NetworkModule {
 
+  static final String HEADER_CACHE_CONTROL = "Cache-Control";
+  static final String HEADER_PRAGMA = "Pragma";
+
   @Provides
   @Singleton
-  static Call.Factory provideOkHttp() {
-    return new OkHttpClient.Builder().build();
+  static ConnectivityManager provideConnectivityManager(Context context) {
+    return (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+  }
+
+  @Provides
+  @Singleton
+  static NetworkInfo provideNetworkInfo(ConnectivityManager connectivityManager) {
+    return connectivityManager.getActiveNetworkInfo();
+  }
+
+  @Provides
+  @Singleton
+  static Call.Factory provideOkHttp(Cache cache,
+      HttpLoggingInterceptor httpLoggingInterceptor,
+      @Named("offline_interceptor") Interceptor offlineInterceptor,
+      @Named("network_interceptor") Interceptor networkInterceptor) {
+    return new OkHttpClient.Builder()
+        .cache(cache)
+        .addInterceptor(httpLoggingInterceptor)
+        .addNetworkInterceptor(networkInterceptor) // only intercepts when network is available
+        .addInterceptor(offlineInterceptor)
+        .build();
+  }
+
+  @Provides
+  @Singleton
+  static Cache provideCache(Context context, @Named("cache_size") long cacheSize) {
+    return new Cache(new File(context.getCacheDir(), "retrofit_cache"), cacheSize);
+  }
+
+  @Provides
+  @Singleton
+  static HttpLoggingInterceptor provideHttpLoggingInterceptor() {
+    HttpLoggingInterceptor httpLoggingInterceptor =
+        new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+          @Override
+          public void log(String message) {
+            Timber.d("http log: " + message);
+          }
+        });
+    httpLoggingInterceptor.level(Level.BASIC);
+    return httpLoggingInterceptor;
+  }
+
+  @Provides
+  @Named("offline_interceptor")
+  @Singleton
+  static Interceptor provideOfflineInterceptor(NetworkInfo networkInfo) {
+    return chain -> {
+      Timber.d("offline interceptor: called.");
+
+      String cacheHeaderValue = networkInfo.isConnected()
+          ? "public, max-age=6000"
+          : "public, only-if-cached, max-stale=6000";
+
+      Request request = chain.request();
+      Response response = chain.proceed(request);
+
+      return response.newBuilder()
+          .removeHeader(HEADER_PRAGMA)
+          .removeHeader(HEADER_CACHE_CONTROL)
+          .header(HEADER_CACHE_CONTROL, cacheHeaderValue)
+          .build();
+    };
+  }
+
+  @Provides
+  @Named("network_interceptor")
+  @Singleton
+  static Interceptor provideNetworkInterceptor(NetworkInfo networkInfo) {
+    return chain -> {
+      Timber.d("network interceptor: called.");
+
+      String cacheHeaderValue = networkInfo.isConnected()
+          ? "public, max-age=6000"
+          : "public, only-if-cached, max-stale=6000";
+
+      Request originalRequest = chain.request();
+      Request request = originalRequest.newBuilder().build();
+      Response response = chain.proceed(request);
+
+      return response.newBuilder()
+          .removeHeader(HEADER_PRAGMA)
+          .removeHeader(HEADER_CACHE_CONTROL)
+          .header(HEADER_CACHE_CONTROL, cacheHeaderValue)
+          .build();
+    };
+  }
+
+  @Provides
+  @Named("cache_size")
+  static long provideCacheSize() {
+    return 5 * 1024 * 1024; // 5MB
   }
 
   @Provides
